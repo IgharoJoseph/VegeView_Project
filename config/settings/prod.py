935e5b603a9bd7a009cc25c99286596659033561
IgharoJoseph <josephigharo@gmail.com>
2024-08-30 12:51:26 +0100

    Third Commit

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,7 +3,10 @@ import dj_database_url
 
 
 DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = os.getenv('HOST')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOST','')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOST', '').split(',')
+ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS if host.strip()]
+
 SECRET_KEY = os.getenv('SECRET_KEY')
 DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
