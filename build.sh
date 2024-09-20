2489a61c0c4b52c50bc634734b653f7ccbcd6294
IgharoJoseph <josephigharo@gmail.com>
2024-09-20 12:51:49 +0100

    I added createuser function in manage.py

--- build.sh
+++ build.sh
@@ -9,9 +9,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Create a superuser if not exists using environment variables
-if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
-    python manage.py createsuperuser \
-    --noinput \
-    --username "$DJANGO_SUPERUSER_USERNAME" \
-    --email "$DJANGO_SUPERUSER_EMAIL" || true
-fi
+python manage.py create_superuser --no-input
