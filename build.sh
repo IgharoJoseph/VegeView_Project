6281ea80f5e2eb11c70948d9a9da160c357e7697
IgharoJoseph <josephigharo@gmail.com>
2024-09-20 16:35:03 +0100

    I added some corrections to the function in build.sh

--- build.sh
+++ build.sh
@@ -8,10 +8,15 @@ echo "Installing requirements..."
 pip install -r requirements.txt
 
 echo "Collecting static files..."
-python manage.py collectstatic --no-input >> build.log 2>&1
+python manage.py collectstatic --no-input 
 
 echo "Running migrations..."
-python manage.py migrate >> build.log 2>&1
+python manage.py migrate 
 
 echo "Creating superuser..."
-python manage.py createsuperuser >> build.log 2>&1
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
+    python manage.py createsuperuser \
+    --noinput \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL" || true
+fi
