3bd1e039c953e7f20efe5836aaca2a710341b302
Joseph Igharo <59059439+IgharoJoseph@users.noreply.github.com>
2026-09-17 17:23:57 +0100

    Add LOGIN_URL and redirect settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -118,3 +118,9 @@ STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Authentication settings
+LOGIN_URL = 'Login'
+LOGIN_REDIRECT_URL = 'DiseaseDirectory'
+LOGOUT_REDIRECT_URL = 'HomePage'
