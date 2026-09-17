c31856c2ea756e6c9fe128062814521d84aad1e4
Joseph Igharo <59059439+IgharoJoseph@users.noreply.github.com>
2026-09-17 17:19:56 +0100

    feat: add vegeviewapp/urls.py for vegetable pest and disease directory

--- vegeviewapp/urls.py
+++ vegeviewapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name="HomePage"),
     path('home', views.index, name="HomePage"),
+    path('directory/', views.directory, name="DiseaseDirectory"),
+    path('directory/<int:pk>/', views.disease_detail, name="DiseaseDetail"),
     path('signup', views.signup, name="Signup"),
     path('login', views.login, name="Login"),
     path('forgotpassword', views.forgotpassword, name="Forgot_Password"),
