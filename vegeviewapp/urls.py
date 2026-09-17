ec65216d98cc782b0f51f81dd5cf07ef4a141279
Joseph Igharo <59059439+IgharoJoseph@users.noreply.github.com>
2026-09-17 19:57:12 +0100

    Register /fields/ URL route for FieldMap

--- vegeviewapp/urls.py
+++ vegeviewapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('directory/', views.directory, name="DiseaseDirectory"),
     path('directory/<int:pk>/', views.disease_detail, name="DiseaseDetail"),
     path('diagnose/', views.diagnose, name="SymptomChecker"),
+    path('fields/', views.field_map_view, name="FieldMap"),
     path('signup', views.signup, name="Signup"),
     path('login', views.login, name="Login"),
     path('logout', views.logout_view, name="Logout"),
