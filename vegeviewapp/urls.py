f526b1aaebb70ee87bedb7f3862bbab1e416e465
Joseph Igharo <59059439+IgharoJoseph@users.noreply.github.com>
2026-09-17 19:43:21 +0100

    Add SymptomChecker route to urls.py

--- vegeviewapp/urls.py
+++ vegeviewapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('home', views.index, name="HomePage"),
     path('directory/', views.directory, name="DiseaseDirectory"),
     path('directory/<int:pk>/', views.disease_detail, name="DiseaseDetail"),
+    path('diagnose/', views.diagnose, name="SymptomChecker"),
     path('signup', views.signup, name="Signup"),
     path('login', views.login, name="Login"),
     path('logout', views.logout_view, name="Logout"),
